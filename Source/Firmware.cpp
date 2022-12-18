77ca7c604e3ea7d9e6c8064294ac876fb02cdfda
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2022-12-17 23:14:17 -0500

    General refactoring

--- Source/Firmware.cpp
+++ Source/Firmware.cpp
@@ -28,10 +28,8 @@ Drop_On_Demand drop_on_demand;
 unsigned long saved_time_0;
 unsigned long saved_time_1;
 
-byte i2c_message;
 
-
-void receiveEvent ( int );
+void onMessage ( int );
 
 
 /**
@@ -45,7 +43,7 @@ void setup (){
     Serial.begin(9600);
 
     Wire.begin(9);
-    Wire.onReceive(receiveEvent);
+    Wire.onReceive(onMessage);
 
     pinMode(tool_0,OUTPUT);
     pinMode(tool_1,OUTPUT);
@@ -54,44 +52,130 @@ void setup (){
 }
 
 
+byte lastMessage (){
+
+    byte message;
+
+    while ( Wire.available() )
+        message = Wire.read();
+
+    return message;
+}
+
+
 //read i2c message
 //check which tool bit is set
 
-void receiveEvent ( int ){
+void onMessage ( int ){
 
-    while ( Wire.available() )
-        i2c_message = Wire.read();
+    const byte message =
+        lastMessage();
 
-    if (bitRead(i2c_message, 0) == 1 ) {
+
+    if(bitRead(message,0)){
         drop_on_demand = on_1;
         shoot_0 = true;
     }
 
-    if (bitRead(i2c_message, 1) == 1 ) {
+    if(bitRead(message,1)){
         drop_on_demand = on_1;
         shoot_1 = true;
     }
 
-    if (bitRead(i2c_message, 2) == 1 ) {
+    if(bitRead(message,2)){
         drop_on_demand = on_1;
         shoot_2 = true;
     }
 
-    if (bitRead(i2c_message, 3) == 1 ) {
+    if(bitRead(message,3)){
         drop_on_demand = on_1;
         shoot_3 = true;
     }
 
-    auto_shoot_0 = bitRead(i2c_message,4);
-    auto_shoot_1 = bitRead(i2c_message,5);
-    auto_shoot_2 = bitRead(i2c_message,6);
-    auto_shoot_3 = bitRead(i2c_message,7);
+    auto_shoot_0 = bitRead(message,4);
+    auto_shoot_1 = bitRead(message,5);
+    auto_shoot_2 = bitRead(message,6);
+    auto_shoot_3 = bitRead(message,7);
 }
 
 
-void loop (){
+void dropAtFrequency ();
+void dropOnDemand ();
 
-    //eject drops at set frequency
+void loop (){
+    dropAtFrequency();
+    dropOnDemand();
+}
+
+
+/**
+ *  @brief  Eject drops on demand
+ */
+
+void dropOnDemand (){
+
+    switch ( drop_on_demand ){
+    case idle_1:
+
+        saved_time_1 = millis();
+
+        shoot_0 = false;
+        shoot_1 = false;
+        shoot_2 = false;
+        shoot_3 = false;
+
+        return;
+    case on_1:
+
+        if(millis() - saved_time_1 < 5){
+
+            if(shoot_0)
+                digitalWrite(tool_0,HIGH);
+
+            if(shoot_1)
+                digitalWrite(tool_1,HIGH);
+
+            if(shoot_2)
+                digitalWrite(tool_2,HIGH);
+
+            if(shoot_3)
+                digitalWrite(tool_3,HIGH);
+
+        } else {
+            drop_on_demand = off_1;
+        }
+
+        return;
+    case off_1:
+
+        if (millis() - saved_time_1 < 50){
+
+            if(shoot_0)
+                digitalWrite(tool_0,LOW);
+
+            if(shoot_1)
+                digitalWrite(tool_1,LOW);
+
+            if(shoot_2)
+                digitalWrite(tool_2,LOW);
+
+            if(shoot_3)
+                digitalWrite(tool_3,LOW);
+
+        } else {
+            drop_on_demand = idle_1;
+        }
+
+        return;
+    }
+}
+
+
+/**
+ *  @brief  Eject drops at a set frequency
+ */
+
+void dropAtFrequency (){
 
     switch ( auto_drop ){
     case on_0 :
@@ -113,7 +197,7 @@ void loop (){
             auto_drop = off_0;
         }
 
-        break;
+        return;
     case off_0:
 
         if (millis() - saved_time_0 < 100){
@@ -135,64 +219,6 @@ void loop (){
             saved_time_0 = millis();
         }
 
-        break;
-    }
-
-
-    // eject drop on demand
-
-    switch ( drop_on_demand ){
-    case idle_1:
-
-        saved_time_1 = millis();
-
-        shoot_0 = false;
-        shoot_1 = false;
-        shoot_2 = false;
-        shoot_3 = false;
-
-        break;
-    case on_1:
-
-        if(millis() - saved_time_1 < 5){
-
-            if(shoot_0)
-                digitalWrite(tool_0,HIGH);
-
-            if(shoot_1)
-                digitalWrite(tool_1,HIGH);
-
-            if(shoot_2)
-                digitalWrite(tool_2,HIGH);
-
-            if(shoot_3)
-                digitalWrite(tool_3,HIGH);
-
-        } else {
-            drop_on_demand = off_1;
-        }
-
-        break;
-    case off_1:
-
-        if (millis() - saved_time_1 < 50){
-
-            if(shoot_0)
-                digitalWrite(tool_0,LOW);
-
-            if(shoot_1)
-                digitalWrite(tool_1,LOW);
-
-            if(shoot_2)
-                digitalWrite(tool_2,LOW);
-
-            if(shoot_3)
-                digitalWrite(tool_3,LOW);
-
-        } else {
-            drop_on_demand = idle_1;
-        }
-
-        break;
+        return;
     }
 }
