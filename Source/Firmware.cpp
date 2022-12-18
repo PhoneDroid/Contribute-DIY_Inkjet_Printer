5e97a6448e8f092b866c3144b0eff6ef7c9e3d50
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2022-12-17 23:40:17 -0500

    General refactoring

--- Source/Firmware.cpp
+++ Source/Firmware.cpp
@@ -9,24 +9,27 @@
 #define tool_2 3
 #define tool_3 2
 
-boolean shoot_0 = false;
-boolean shoot_1 = false;
-boolean shoot_2 = false;
-boolean shoot_3 = false;
+boolean
 
-boolean auto_shoot_0 = false;
-boolean auto_shoot_1 = false;
-boolean auto_shoot_2 = false;
-boolean auto_shoot_3 = false;
+    drop_atFrequency = true ,
+    drop_onDemand = false ,
 
-enum Auto_Drop {on_0, off_0};
-Auto_Drop auto_drop;
+    auto_shoot_0 ,
+    auto_shoot_1 ,
+    auto_shoot_2 ,
+    auto_shoot_3 ,
 
-enum Drop_On_Demand {idle_1, on_1, off_1};
-Drop_On_Demand drop_on_demand;
+    shoot_0 ,
+    shoot_1 ,
+    shoot_2 ,
+    shoot_3 ,
 
-unsigned long saved_time_0;
-unsigned long saved_time_1;
+    idle = true ;
+
+
+unsigned long
+    saved_time_0 ,
+    saved_time_1 ;
 
 
 void onMessage ( int );
@@ -63,8 +66,12 @@ byte lastMessage (){
 }
 
 
-//read i2c message
-//check which tool bit is set
+void demandDropping ();
+
+
+/**
+ *  @brief  Received I2C messages determine which tool bit is set
+ */
 
 void onMessage ( int ){
 
@@ -72,25 +79,20 @@ void onMessage ( int ){
         lastMessage();
 
 
-    if(bitRead(message,0)){
-        drop_on_demand = on_1;
+    if(bitRead(message,0))
         shoot_0 = true;
-    }
 
-    if(bitRead(message,1)){
-        drop_on_demand = on_1;
+    if(bitRead(message,1))
         shoot_1 = true;
-    }
 
-    if(bitRead(message,2)){
-        drop_on_demand = on_1;
+    if(bitRead(message,2))
         shoot_2 = true;
-    }
 
-    if(bitRead(message,3)){
-        drop_on_demand = on_1;
+    if(bitRead(message,3))
         shoot_3 = true;
-    }
+
+    if(shoot_0 || shoot_1 || shoot_2 || shoot_3)
+        demandDropping();
 
     auto_shoot_0 = bitRead(message,4);
     auto_shoot_1 = bitRead(message,5);
@@ -103,8 +105,37 @@ void dropAtFrequency ();
 void dropOnDemand ();
 
 void loop (){
+
     dropAtFrequency();
-    dropOnDemand();
+
+    if(drop_onDemand)
+        dropOnDemand();
+}
+
+
+void drop ( boolean state ){
+
+    if(shoot_0)
+        digitalWrite(tool_0,state);
+
+    if(shoot_1)
+        digitalWrite(tool_1,state);
+
+    if(shoot_2)
+        digitalWrite(tool_2,state);
+
+    if(shoot_3)
+        digitalWrite(tool_3,state);
+}
+
+
+
+void demandDropping (){
+
+    drop(true);
+
+    saved_time_1 = millis();
+    drop_onDemand = true;
 }
 
 
@@ -114,61 +145,27 @@ void loop (){
 
 void dropOnDemand (){
 
-    switch ( drop_on_demand ){
-    case idle_1:
+    const long delta =
+        millis() - saved_time_1;
 
-        saved_time_1 = millis();
-
-        shoot_0 = false;
-        shoot_1 = false;
-        shoot_2 = false;
-        shoot_3 = false;
-
+    if(delta < 5)
         return;
-    case on_1:
 
-        if(millis() - saved_time_1 < 5){
+    drop(false);
 
-            if(shoot_0)
-                digitalWrite(tool_0,HIGH);
-
-            if(shoot_1)
-                digitalWrite(tool_1,HIGH);
-
-            if(shoot_2)
-                digitalWrite(tool_2,HIGH);
-
-            if(shoot_3)
-                digitalWrite(tool_3,HIGH);
-
-        } else {
-            drop_on_demand = off_1;
-        }
-
+    if(delta < 50)
         return;
-    case off_1:
 
-        if (millis() - saved_time_1 < 50){
+    drop_onDemand = false;
 
-            if(shoot_0)
-                digitalWrite(tool_0,LOW);
-
-            if(shoot_1)
-                digitalWrite(tool_1,LOW);
-
-            if(shoot_2)
-                digitalWrite(tool_2,LOW);
-
-            if(shoot_3)
-                digitalWrite(tool_3,LOW);
-
-        } else {
-            drop_on_demand = idle_1;
-        }
-
-        return;
-    }
+    shoot_0
+    = shoot_1
+    = shoot_2
+    = shoot_3
+    = false;
 }
+
+
 
 
 /**
@@ -177,48 +174,48 @@ void dropOnDemand (){
 
 void dropAtFrequency (){
 
-    switch ( auto_drop ){
-    case on_0 :
+    const long delta =
+        millis() - saved_time_0;
 
-        if(millis() - saved_time_0 < 50){
 
-            if(auto_shoot_0)
-                digitalWrite(tool_0, HIGH);
+    if(drop_atFrequency){
 
-            if(auto_shoot_1)
-                digitalWrite(tool_1, HIGH);
-
-            if(auto_shoot_2)
-                digitalWrite(tool_2, HIGH);
-
-            if(auto_shoot_3)
-                digitalWrite(tool_3, HIGH);
-        } else {
-            auto_drop = off_0;
+        if(delta <= 50){
+            drop_atFrequency = false;
+            return;
         }
 
-        return;
-    case off_0:
+        if(auto_shoot_0)
+            digitalWrite(tool_0,HIGH);
 
-        if (millis() - saved_time_0 < 100){
+        if(auto_shoot_1)
+            digitalWrite(tool_1,HIGH);
 
-            if(auto_shoot_0)
-                digitalWrite(tool_0,LOW);
+        if(auto_shoot_2)
+            digitalWrite(tool_2,HIGH);
 
-            if(auto_shoot_1)
-                digitalWrite(tool_1,LOW);
-
-            if(auto_shoot_2)
-                digitalWrite(tool_2,LOW);
-
-            if(auto_shoot_3)
-                digitalWrite(tool_3,LOW);
-
-        } else {
-            auto_drop = on_0;
-            saved_time_0 = millis();
-        }
+        if(auto_shoot_3)
+            digitalWrite(tool_3,HIGH);
 
         return;
     }
+
+
+    if (delta >= 100){
+        drop_atFrequency = true;
+        saved_time_0 = millis();
+        return;
+    }
+
+    if(auto_shoot_0)
+        digitalWrite(tool_0,LOW);
+
+    if(auto_shoot_1)
+        digitalWrite(tool_1,LOW);
+
+    if(auto_shoot_2)
+        digitalWrite(tool_2,LOW);
+
+    if(auto_shoot_3)
+        digitalWrite(tool_3,LOW);
 }
